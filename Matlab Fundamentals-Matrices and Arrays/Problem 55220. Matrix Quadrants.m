9fbab55943464041538cca1aedf8822f4aa7e572
omprakashb91 <79237022+omprakashb91@users.noreply.github.com>
2022-10-23 09:33:16 +0530

    prob_5230_commit_1

---
+++ Matlab Fundamentals-Matrices and Arrays/Problem 55220. Matrix Quadrants.m
@@ -0,0 +1,6 @@
+% Problem 55220
+% Matrix Quadrants
+
+function M = foursquare(N)
+M = [ones(N,N) zeros(N,N) ; zeros(N,N) N*ones(N,N)] ;
+end
---
+++ Matlab Fundamentals-Matrices and Arrays/Problem 55230. Find Closest Constant.m
@@ -0,0 +1,20 @@
+% Problem 55230
+% Find Closest Constant
+
+% Method-1
+function cc = closestconst(x)
+r= [0 1 sqrt(2) exp(1) pi (2*pi)];
+d = abs( x - r )
+[M,I] = min(d)
+cc=r(I)
+end
+
+% Method-2
+%function cc = closestconst(x)
+%r= [0 1 sqrt(2) exp(1) pi (2*pi)];
+%for a=1:length(r)
+%   d(a) = abs(x-r(a));
+%end
+%[M,I] = min(d)
+%cc=r(I)
+%end
