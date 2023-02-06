f8fc74081e80ff99e09f0a1a2f6b906279270f1b
omprakashb91 <79237022+omprakashb91@users.noreply.github.com>
2023-02-06 06:46:59 +0530

    Rev1_commit

--- Cody Challenge/Problem 95 Given two arrays, find the maximum overlap.m
+++ Cody Challenge/Problem 95 Given two arrays, find the maximum overlap.m
@@ -1,16 +1,43 @@
-%Problem 95
-%Given two arrays, find the maximum overlap
-%This code provides solution if the overlap happens extreme ends not inbetween the array
 %Date: 05 Feb 2023
+%Revision1
+%Accepted solution
 function s3 = overlap(s1,s2)
 clc;
+
 m=length(s1);
 n=length(s2);
     if m>n
         A=s1; B=s2;
+
     else
-        A=s2; B=s1;
+        A=s2;
+        B=s1;
+        m=length(A);
+        n=length(B);
     end
+
+    la=length(A);
+    lb=length(B);
+    prediff=0;
+    s3=[];
+%Preloop1
+    while(lb<m|lb==m)
+        C = A(prediff+1:lb);
+        D = B;
+        if C==D
+            s3=A;
+            break;
+         else
+            prediff=prediff+1; %0+1=1
+            lb=lb+1;
+        end
+    end
+
+%Tf = isempty(s3);
+if isempty(s3)==false
+    s3;
+else
+
 %Loop1 TAIL PORTION
     la=length(A);
     lb=length(B);
@@ -48,4 +75,5 @@ n=length(s2);
     else
         s3=Ans2;
     end
+end
 end
